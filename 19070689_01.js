b778444ac5a1e59217d331e56ca9dbfb5df48165
Fatima129 <92490346+Fatima129@users.noreply.github.com>
2022-12-03 20:55:52 -0600

    Add files via upload

---
+++ 19070689_01.js
@@ -0,0 +1,7 @@
+let nombre='         Fatima Guadalupe Acosta Mata               '
+console.log("uso de trim");
+console.log(nombre.trim());
+console.log("uso de trim start");
+console.log(nombre.trimStart());
+console.log("uso de trim end:");
+console.log(nombre.trimEnd());
